34fe16995a78076b542b179b7b5ce85ebc6b5bf9
Knurlnheim <marcfun@free.fr>
2025-07-28 11:12:09 +0200

    Switch to Axios, handle multiple type of bulletin

--- MMM-alerte-meteo.js
+++ MMM-alerte-meteo.js
@@ -3,31 +3,30 @@
 Module.register("MMM-alerte-meteo", {
   defaults: {
     title: 'Alertes Météo',
-    url: '',
+    baseUrl: 'https://public-api.meteofrance.fr',
+    apiPath: '/public/DPVigilance/v1/textesvigilance/encours',
+    apikey: '',
     updateInterval: 30*60*1000,
-    dptsList: [{id: '59', label: 'Nord'}]
+    dptsList: ['59']
     },
 
   start: function() {
     this.getData();
     this.scheduleUpdate();
     this.vigilances = [];
-
   },
 
   getStyles: function() {
   	return ["alertstyle.css"];
   },
 
-
-
   risques: [
     'Aucun',
     'Vent',
-    'Pluie-Inondation',
+    'Pluie',
     'Orages',
-    'Inondation',
-    'Neige',
+    'Crues',
+    'Neige / Verglas',
     'Canicule',
     'Grand Froid',
     'Avalanches',
@@ -35,7 +34,7 @@ Module.register("MMM-alerte-meteo", {
   ],
 
   risquesIcons: [
-    'Aucun',
+    'None',
     'wi-strong-wind',
     'wi-rain',
     'wi-thunderstorm',
@@ -69,15 +68,38 @@ Module.register("MMM-alerte-meteo", {
     var table = document.createElement("table");
     table.className = "medium";
     for (var i in this.vigilances) {
-      if (parseInt(this.vigilances[i].couleur) > 1) {
-        var color = this.colorClass[this.vigilances[i].couleur];
+//	 Log.info(this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION_ZONAL" ));
+
+
+      if (this.vigilances[i].bloc_items.length != 0 && ( typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_SITUATION" ) !== 'undefined' || typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_SUIVI" ) !== 'undefined' || typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION" ) !== 'undefined' || typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION_ZONAL" ) !== 'undefined' )) {
+      //if (this.vigilances[i].bloc_items.length > 0) {
+        let dept_id = this.vigilances[i].domain_id
+        let dept_name = this.vigilances[i].domain_name
+	if (typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_SUIVI" ) !== 'undefined' ) {
+          var Qualif = this.vigilances[i].bloc_items.find(x => x.id === "DEP_SUIVI" );
+        } else if (typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION" ) !== 'undefined') {
+          var Qualif = this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION" );
+        } else if (typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_SITUATION" ) !== 'undefined') {
+          var Qualif = this.vigilances[i].bloc_items.find(x => x.id === "DEP_SITUATION" );
+        } else if (typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION_ZONAL" ) !== 'undefined' ) { 
+          var Qualif = this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION_ZONAL" );
+        };
+        let hazard_code =  Qualif.text_items[0].hazard_code
+        //Log.info("hazard_code :" + hazard_code);
+        // let color = Qualif.text_items[0].term_items[0].risk_code
+
+        
+
+        
+        var color = this.colorClass[Qualif.text_items[0].term_items[0].risk_code];
 
         var row = document.createElement("tr");
         table.appendChild(row);
 
         var cell = document.createElement("td");
-        var risqueIdx = parseInt(this.vigilances[i].risque);
-        cell.innerHTML = this.vigilances[i].label + " ("+this.vigilances[i].dept+")" ;
+        var risqueIdx = parseInt(hazard_code);
+        //Log.info("risqueIdx :" + risqueIdx);
+        cell.innerHTML =  dept_name+ " ("+dept_id+")" ;
         cell.className = "dept " + color;
         row.appendChild(cell);
 
@@ -109,39 +131,72 @@ Module.register("MMM-alerte-meteo", {
   },
 
   getData: function () {
-    this.sendSocketNotification('GET_VIGILANCE', this.config.dptsList);
+    var myconf = {};
+    myconf.baseUrl= this.config.baseUrl;
+    myconf.apiPath = this.config.apiPath;
+    myconf.apikey = this.config.apikey;
+    myconf.dptsList = this.config.dptsList;
+    // Log.info(myconf) ;
+   this.sendSocketNotification('GET_VIGILANCE', myconf);
 //    this.sendSocketNotification('GET_VIGILANCE', this.config.departements);
     this.loaded = false;
   },
 
   socketNotificationReceived: function(notification, payload) {
-    if (notification === "DATA_VIGILANCE") {
-      //Log.info("received :"+ payload);
-      this.vigilances = payload;
+  //    Log.info("received :"+ notification);
+  //    Log.info("received :"+ payload);
       this.loaded = true;
+    if (notification === "DATA_VIGILANCE") {
+      Log.info("received :"+ payload);
+      this.vigilances = payload;
+//      this.loaded = true;
       var fade = 500;
       var has_alert = false;
-      this.updateDom(fade);
+      //this.updateDom(fade);
       for (var i in this.vigilances) {
-        if (this.vigilances[i].couleur !== "1") {
+        Log.info(this.vigilances[i].bloc_items.length);
+        if (this.vigilances[i].bloc_items.length != 0 && (typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_SUIVI" ) !== 'undefined' || typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_SITUATION" ) !== 'undefined' || typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION_ZONAL") !== 'undefined')) {
+        //if (this.vigilances[i].bloc_items.length != 0 && typeof this.vigilances[i].bloc_items.find(x => x.id === "DEP_SUIVI" ) !== 'undefined') {
           has_alert = true;
-          //Log.info("has_alert :"+ has_alert) ;
+           //Log.info(this.vigilances[i].bloc_items.length);
+      //     Log.info("has_alert :"+ has_alert) ;
         }
+      //   Log.info(this.vigilances[i].bloc_items.find(x => x.id === "DEP_QUALIFICATION_ZONAL" ));
       }
+
+
+      // for (var i in this.vigilances) {
+      //   if (this.vigilances[i].couleur !== "1") {
+      //     has_alert = true;
+      //     //Log.info("has_alert :"+ has_alert) ;
+      //   }
+      // }
       if (has_alert === true) {
         if (this.hidden == true ) {
-          this.show(1000, {lockString: this.identifier});
+//          this.show(1000, {lockString: this.identifier});
+          this.show(1000, function() {
+          });
+
           //Log.info("hidden was true") ;
         }
         this.updateDom(fade);
         //Log.info("updateDom") ;
       } else {
         if (this.hidden == false ) {
-          this.hide(1000, {lockString: this.identifier});
+          //this.hide(1000, {lockString: this.identifier});
+          this.hide(1000, function() {
+		//Module hidden
+	  });
           //Log.info("hidden was false") ;
         }
       }
     }
+       else if (notification === "DATA_VIGILANCE_ERROR") {
+//          Log.info("hide") ;
+          this.hide(1000, function() {
+                //Module hidden
+          });
+}
 
   }
 
--- node_helper.js
+++ node_helper.js
@@ -1,67 +1,108 @@
 var NodeHelper = require('node_helper');
-var request = require('request');
-var convert = require('xml-js');
+var axios = require('axios');
+
 
 module.exports = NodeHelper.create({
   start: function () {
     console.log('MMM-alerte-meteo helper started...');
   },
 
-  getViligance: function (deptsList) {
-      var self = this;
-      request({ url: 'http://www.vigimeteo.com/data/NXFR33_LFPW_.xml', method: 'GET' }, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var vigiResult = convert.xml2js(body, {compact: true, attributesKey: 'attributes', textKey: 'value', cdataKey: 'value', commentKey: 'value', ignoreDeclaration: true});
-            var deptResult = vigiResult.CV.DV;
-            //var deptsList = ["59", "62", "57", "27", "32"];
-            var result = [];
-            for (var j in deptsList) {
-            // console.log("id " + deptsList[j].id + " label " + deptsList[j].label);
-              for (var i in deptResult) {
-                if (deptResult[i].attributes.dep === deptsList[j].id) {
-                  var dept = deptResult[i].attributes.dep;
-                  var couleur = deptResult[i].attributes.coul;
-                  // console.log("id " + deptsList[j].id +" couleur "+ deptResult[i].attributes.coul);
-                  // console.log(Array.isArray(deptResult[i].risque));
-                  if (parseInt(couleur) > 1 )  {
-                  //if (typeof deptResult[i].risque !== "undefined" && deptResult[i].risque !== null)  {
-                    if (Array.isArray(deptResult[i].risque)) {
-                    //  console.log("its an array of " + deptResult[i].risque.length);
-                      for (var n in deptResult[i].risque) {
-                    //    console.log("iteration " + n);
-                        var risque = deptResult[i].risque[n].attributes.val;
-                        result_item = {'dept': dept, 'label': deptsList[j].label, 'couleur': couleur, 'risque': risque };
-                    //    console.log(result_item);
-                        result.push(result_item);
-                      }
-                    } else {
-                      var risque = deptResult[i].risque.attributes.val;
-                      result_item = {'dept': dept, 'label': deptsList[j].label, 'couleur': couleur, 'risque': risque };
-                      result.push(result_item);
-                    }
-                  } else {
-                    var  risque ="0";
-                    result_item = {'dept': dept, 'label': deptsList[j].label, 'couleur': couleur, 'risque': risque };
-                        result.push(result_item);
-                  }
-                  //console.log(result[j]) ; // {a: 5, b: 6}
-                }
-              }
-            }
 
-            self.sendSocketNotification('DATA_VIGILANCE', result);
 
-          }
-      });
-  },
+
+getViligance: function(url) {
+    var self = this;
+//     console.log(self.config.apiPath); 
+//     console.log(self.config.baseUrl); 
+//     console.log(self.config.apikey); 
+//     console.log(self.config.dptsList); 
+    
+axios({
+  url: self.config.apiPath,
+  baseURL: self.config.baseUrl,
+  method: 'get',
+  headers: {'apikey': self.config.apikey}
+  })
+.then(function (response) {
+//  console.log(response);
+  if(response.status == 200 && response.data) {
+//    console.log(response.data);
+    var VigilanceData=[];
+    for (var j in self.config.dptsList) {
+      // console.log(self.config.dptsList[j]); 
+      VigilanceData.push(response.data.product.text_bloc_items.find(x => x.domain_id === self.config.dptsList[j] ));
+    }
+ //   console.log(VigilanceData); 
+    self.sendSocketNotification("DATA_VIGILANCE", VigilanceData);
+  } else {
+    self.sendSocketNotification("DATA_VIGILANCE_ERROR", 'API error: ' + response.statusText);
+  }
+})
+.catch(function (error) {
+ // console.log(error);
+  self.sendSocketNotification("DATA_VIGILANCE_ERROR", error.message);
+});
+},
+
+
+
+  // getViligance: function (deptsList) {
+  //     var self = this;
+  //     request({ url: 'http://www.vigimeteo.com/data/NXFR33_LFPW_.xml', method: 'GET' }, function (error, response, body) {
+  //         if (!error && response.statusCode == 200) {
+  //           var vigiResult = convert.xml2js(body, {compact: true, attributesKey: 'attributes', textKey: 'value', cdataKey: 'value', commentKey: 'value', ignoreDeclaration: true});
+  //           var deptResult = vigiResult.CV.DV;
+  //           //var deptsList = ["59", "62", "57", "27", "32"];
+  //           var result = [];
+  //           for (var j in deptsList) {
+  //           // console.log("id " + deptsList[j].id + " label " + deptsList[j].label);
+  //             for (var i in deptResult) {
+  //               if (deptResult[i].attributes.dep === deptsList[j].id) {
+  //                 var dept = deptResult[i].attributes.dep;
+  //                 var couleur = deptResult[i].attributes.coul;
+  //                 // console.log("id " + deptsList[j].id +" couleur "+ deptResult[i].attributes.coul);
+  //                 // console.log(Array.isArray(deptResult[i].risque));
+  //                 if (parseInt(couleur) > 1 )  {
+  //                 //if (typeof deptResult[i].risque !== "undefined" && deptResult[i].risque !== null)  {
+  //                   if (Array.isArray(deptResult[i].risque)) {
+  //                   //  console.log("its an array of " + deptResult[i].risque.length);
+  //                     for (var n in deptResult[i].risque) {
+  //                   //    console.log("iteration " + n);
+  //                       var risque = deptResult[i].risque[n].attributes.val;
+  //                       result_item = {'dept': dept, 'label': deptsList[j].label, 'couleur': couleur, 'risque': risque };
+  //                   //    console.log(result_item);
+  //                       result.push(result_item);
+  //                     }
+  //                   } else {
+  //                     var risque = deptResult[i].risque.attributes.val;
+  //                     result_item = {'dept': dept, 'label': deptsList[j].label, 'couleur': couleur, 'risque': risque };
+  //                     result.push(result_item);
+  //                   }
+  //                 } else {
+  //                   var  risque ="0";
+  //                   result_item = {'dept': dept, 'label': deptsList[j].label, 'couleur': couleur, 'risque': risque };
+  //                       result.push(result_item);
+  //                 }
+  //                 //console.log(result[j]) ; // {a: 5, b: 6}
+  //               }
+  //             }
+  //           }
+
+  //           self.sendSocketNotification('DATA_VIGILANCE', result);
+
+  //         }
+  //     });
+  // },
 
 
   //Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
-    //console.log("Received :" + notification);
-  //  console.log("Received payload:" + payload);
+    var self = this;
+    // console.log("Received :" + notification);
+    // console.log("Received payload:" + payload);
     if (notification === 'GET_VIGILANCE') {
-      this.getViligance(payload);
+      self.config = payload;
+      this.getViligance();
     }
   }
 
